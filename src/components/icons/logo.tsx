c83a141d805d83fd3fa8ce11554aa8da4e468304
btw Liorne <thiagoliorne17@gmail.com>
2025-10-01 04:53:14 +0000

    https://i.imgur.com/i065eLJ.png (_for element div_)

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("flex items-center gap-2 text-xl font-bold text-white", className)}>
-      <Image src="https://i.imgur.com/F181ozd.png" alt="Fusion Pay Logo" width={24} height={24} />
+      <Image src="https://i.imgur.com/i065eLJ.png" alt="Fusion Pay Logo" width={24} height={24} />
       <span className="font-headline">Fusion Pay</span>
     </div>
   )
