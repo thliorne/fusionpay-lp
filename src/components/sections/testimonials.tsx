b8ce8acada8127081accf00b966bea06f0b169a3
btw Liorne <thiagoliorne17@gmail.com>
2025-10-18 03:28:33 +0000

    essa seção também (_for element section_)

--- src/components/sections/testimonials.tsx
+++ src/components/sections/testimonials.tsx
@@ -18,11 +18,11 @@ function TestimonialsContainer({
   testimonials,
   className 
 }: TestimonialsSectionProps) {
-  const duplicatedTestimonials = [...testimonials, ...testimonials];
+  const duplicatedTestimonials = [...testimonials, ...testimonials, ...testimonials, ...testimonials];
 
   return (
     <section className={cn(
-      "bg-primary text-white",
+      "text-white section-surface",
       "py-12 sm:py-24 md:py-32 px-0",
       className
     )}>
@@ -37,7 +37,7 @@ function TestimonialsContainer({
         </div>
 
         <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
-          <div className="group flex w-max overflow-hidden p-2 [--gap:1rem] [gap:var(--gap)] flex-row [--duration:80s]">
+          <div className="group flex w-max overflow-hidden p-2 [--gap:1rem] [gap:var(--gap)] flex-row [--duration:160s]">
             <div className="flex shrink-0 animate-marquee flex-row items-center justify-around [gap:var(--gap)]">
               {duplicatedTestimonials.map((testimonial, i) => (
                 <TestimonialCard 
@@ -58,8 +58,8 @@ function TestimonialsContainer({
             </div>
           </div>
 
-          <div className="pointer-events-none absolute inset-y-0 left-0 hidden w-1/3 bg-gradient-to-r from-primary sm:block" />
-          <div className="pointer-events-none absolute inset-y-0 right-0 hidden w-1/3 bg-gradient-to-l from-primary sm:block" />
+          <div className="pointer-events-none absolute inset-y-0 left-0 hidden w-1/3 bg-gradient-to-r from-background to-transparent sm:block" />
+          <div className="pointer-events-none absolute inset-y-0 right-0 hidden w-1/3 bg-gradient-to-l from-background to-transparent sm:block" />
         </div>
       </div>
     </section>
