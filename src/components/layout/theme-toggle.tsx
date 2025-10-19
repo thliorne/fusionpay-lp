7aebf319e42272ba86de1905c148fcf9d97a3d29
btw Liorne <thiagoliorne17@gmail.com>
2025-10-19 01:49:04 +0000

    remova (_for element div_)

--- src/components/layout/theme-toggle.tsx
+++ src/components/layout/theme-toggle.tsx
@@ -45,34 +45,7 @@ export function ThemeToggle({ className }: ThemeToggleProps) {
       role="button"
       tabIndex={0}
     >
-      <div className="flex justify-between items-center w-full">
-        <div
-          className={cn(
-            "flex justify-center items-center w-6 h-6 rounded-full transition-transform duration-300",
-            isDark
-              ? "transform translate-x-0 bg-zinc-800"
-              : "transform translate-x-8 bg-gray-200"
-          )}
-        >
-          {isDark ? (
-            <Moon className="w-4 h-4 text-white" strokeWidth={1.5} />
-          ) : (
-            <Sun className="w-4 h-4 text-gray-700" strokeWidth={1.5} />
-          )}
-        </div>
-        <div
-          className={cn(
-            "flex justify-center items-center w-6 h-6 rounded-full transition-transform duration-300",
-            isDark ? "bg-transparent" : "transform -translate-x-8"
-          )}
-        >
-          {isDark ? (
-            <Sun className="w-4 h-4 text-gray-500" strokeWidth={1.5} />
-          ) : (
-            <Moon className="w-4 h-4 text-black" strokeWidth={1.5} />
-          )}
-        </div>
-      </div>
+
     </div>
   )
 }
