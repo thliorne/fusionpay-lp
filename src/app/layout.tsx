8636de873c334ee6830f3a0cc8ebc4c11f89eab6
Thiago Liorne <liornethiago@gmail.com>
2025-10-24 05:20:21 +0000

    torne essa imagem a favicon desse site: https://i.imgur.com/FkANKFD.png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <head>
-        <link rel="icon" href="https://i.imgur.com/cauCgQR.png" />
+        <link rel="icon" href="https://i.imgur.com/FkANKFD.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=swap" rel="stylesheet" />
